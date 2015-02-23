164447d77252a7492289b7fa5c2172416bce0b12
Jan-Hendrik Kuperus <jhkuperus@yoink.nl>
2015-02-24 00:25:57 +0100

    A few fixes and scenarios work fantastically :)

--- src/node/lib_v2/playback.js
+++ src/node/lib_v2/playback.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {};
-module.exports.defaultRequestMatcher          = simpleRequestKeyRequestMatcher;
-module.exports.simpleRequestKeyRequestMatcher = simpleRequestKeyRequestMatcher;
-module.exports.scenarioBasedRequestMatcher    = scenarioBasedRequestMatcher;
+module.exports.defaultRequestMatcher          = combineMatchers(scenarioBasedRequestMatcher, simpleRequestKeyRequestMatcher);
+module.exports.simpleRequestKeyRequestMatcher = combineMatchers(simpleRequestKeyRequestMatcher);
+module.exports.scenarioBasedRequestMatcher    = combineMatchers(scenarioBasedRequestMatcher);
 module.exports.addRecording                   = addSingleRecording;
 module.exports.addScenario                    = addSingleScenario;
 module.exports.exportRecordings               = exportRecordings;
@@ -14,13 +14,52 @@ var recordings = {};
 var scenarios = {};
 var scenarioEndpointBindings = {};
 
+function combineMatchers() {
+
+    var matchers = unwrapMatchers(Array.prototype.slice.call(arguments));
+    if (matchers.length === 0) { throw Error('Need at least one matcher!'); }
+
+    var matcherFunction = function(reqContext) {
+        var d = $q.defer();
+
+        for (var i = 0; i < matchers.length && !reqContext.hasOwnProperty('playbackResponse'); i++) {
+            matchers[i](reqContext);
+        }
+
+        d.resolve(reqContext);
+        return d.promise;
+    };
+
+    matcherFunction.matchers = matchers;
+
+    return matcherFunction;
+
+}
+
+function unwrapMatchers(matchers) {
+    var unwrappedMatchers = [];
+
+    for (var i = 0; i < matchers.length; i++) {
+        if (matchers[i].matchers) {
+            unwrappedMatchers = unwrappedMatchers.concat(matchers[i].matchers);
+        }
+        else {
+            unwrappedMatchers.push(matchers[i]);
+        }
+    }
+
+    return unwrappedMatchers;
+}
+
+// --- Simple recording playback
+
 function addSingleRecording (endpoint, requestKey, recordedResponse) {
 
-    // ensure presence of endpoint
-    recordings[endpoint] = recordings[endpoint] || {};
-    
-    recordings[endpoint][requestKey] = recordedResponse;
-    
+  // ensure presence of endpoint
+  recordings[endpoint] = recordings[endpoint] || {};
+
+  recordings[endpoint][requestKey] = recordedResponse;
+
 }
 
 function simpleRequestKeyRequestMatcher (reqContext) {
@@ -44,6 +83,8 @@ function simpleRequestKeyRequestMatcher (reqContext) {
 
 }
 
+// --  Scenario based playback (includes adding scenarios to the datastore)
+
 function scenarioBasedRequestMatcher(reqContext) {
     var deferred = $q.defer();
     
@@ -62,7 +103,7 @@ function scenarioBasedRequestMatcher(reqContext) {
     if (potentialScenarios.length > 0) {
         // Take the first matching scenario and use its response
         reqContext.playbackResponse = potentialScenarios[0].currentPosition.state.response;
-        console.log('|    Playing scenario request: ' + potentialScenarios[0].scenario.title + ' -- ' + potentialScenarios[0].currentPosition.title);
+        console.log('|    Playing scenario request: ' + potentialScenarios[0].scenario.title + ' -- ' + potentialScenarios[0].currentPosition.state.title);
         deregisterScenarioFromEndpoint(potentialScenarios[0], potentialScenarios[0].currentPosition.state.endpointKey);
         potentialScenarios[0].next();
         if (!potentialScenarios[0].finished) {
@@ -98,8 +139,15 @@ function registerScenarioOnEndpoint(scenarioPlayer, endpoint) {
     scenarioEndpointBindings[endpoint][scenarioPlayer.$$scenarioKey] = scenarioPlayer;
 }
 
+// --- Export data
 function exportRecordings () {
 
     return recordings;
+
+}
+
+function exportScenarios() {
+
+    return scenarios;
 
 }
--- src/node/test/test_v2.js
+++ src/node/test/test_v2.js
@@ -6,53 +6,69 @@ var config = {
     endpoints: {
         'google': {
             targetBaseUrl: 'http://www.google.com/'
+        },
+        'yahoo': {
+            targetBaseUrl: 'http://www.yahoo.com/'
         }
-    },
-    playback: {
-        matcher: $nocca2.playback.scenarioBasedRequestMatcher
     }
 };
 
 var Matchers = {
     anyMatcher: function(reqContext) {
         return true;
+    },
+    ginInSoapBodyBuilder: function(gin) {
+        var ginQuery = '<GIN>' + gin + '</GIN>';
+        return function (reqContext) {
+            return reqContext.request.body.indexOf(ginQuery) || false;
+        };
+    },
+    ginInUrlBuilder: function(gin) {
+        return function (reqContext) {
+            return reqContext.request.url.split('/')[3] === gin || false;
+        };
     }
 };
 
 var GoogleResponses = {
     "f1": {
-        "requestKey": "{\"url\":\"/google/sjampoooo\",\"headers\":[\"accept-encoding:gzip, deflate, sdch\",\"accept-language:en-US,en;q=0.8,nl;q=0.6\",\"accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\",\"cache-control:max-age=0\",\"connection:keep-alive\",\"host:localhost:3003\",\"user-agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36\"],\"method\":\"GET\"}",
-        "hits": 0,
         "statusCode": 404,
         "headers": {
-            "X-Content-Type-Options": "nosniff",
-            "Date": "Mon, 23 Feb 2015 10:15:30 GMT",
-            "Server": "sffe",
-            "X-Xss-Protection": "1; mode=block",
-            "Alternate-Protocol": "80:quic,p=0.08",
-            "Content-Length": "1434",
-            "Content-Type": "text/html; charset=UTF-8",
-            "Via": "1.1 WSA2.klm.com:80 (Cisco-WSA/8.0.6-119)",
-            "Connection": "keep-alive"
+            "content-type": "text/html; charset=UTF-8",
+            "x-content-type-options": "nosniff",
+            "date": "Mon, 23 Feb 2015 21:16:18 GMT",
+            "server": "sffe",
+            "content-length": "1435",
+            "x-xss-protection": "1; mode=block",
+            "alternate-protocol": "80:quic,p=0.08"
         },
-        "body": "<!DOCTYPE html>\n<html lang=en>\n <meta charset=utf-8>\n <meta name=viewport content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n <title>Error 404 (Not Found)!!1</title>\n <style>\n *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/errors/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/errors/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}\n </style>\n <a href=//www.google.com/><span id=logo aria-label=Google></span></a>\n <p><b>404.</b> <ins>That’s an error.</ins>\n <p>The requested URL <code>/sjampoooo</code> was not found on this server. <ins>That’s all we know.</ins>\n"
+        "body": "<!DOCTYPE html>\n<html lang=en>\n  <meta charset=utf-8>\n  <meta name=viewport content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n  <title>Error 404 (Not Found)!!1</title>\n  <style>\n    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/errors/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/errors/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}\n  </style>\n  <a href=//www.google.com/><span id=logo aria-label=Google></span></a>\n  <p><b>404.</b> <ins>Thatâ€™s an error.</ins>\n  <p>The requested URL <code>/sjampooooo</code> was not found on this server.  <ins>Thatâ€™s all we know.</ins>\n"
     },
     "f2": {
-        "requestKey": "{\"url\":\"/google/sjampooooooo/jolo\",\"headers\":[\"accept-encoding:gzip, deflate, sdch\",\"accept-language:en-US,en;q=0.8,nl;q=0.6\",\"accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\",\"cache-control:max-age=0\",\"connection:keep-alive\",\"host:localhost:3003\",\"user-agent:Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36\"],\"method\":\"GET\"}",
-        "hits": 0,
+        "statusCode": 404,
+        "headers": {
+            "content-type": "text/html; charset=UTF-8",
+            "x-content-type-options": "nosniff",
+            "date": "Mon, 23 Feb 2015 21:16:18 GMT",
+            "server": "sffe",
+            "x-xss-protection": "1; mode=block",
+            "content-length": "1435",
+            "alternate-protocol": "80:quic,p=0.08"
+        },
+        "body": "<!DOCTYPE html>\n<html lang=en>\n  <meta charset=utf-8>\n  <meta name=viewport content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n  <title>Error 404 (Not Found)!!1</title>\n  <style>\n    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/errors/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/errors/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}\n  </style>\n  Holadiee, dit is stap 2 ^_^  <ins>Thatâ€™s all we know.</ins>\n"
+    },
+    "f3": {
         "statusCode": 200,
         "headers": {
-            "X-Content-Type-Options": "nosniff",
-            "Date": "Mon, 23 Feb 2015 12:49:24 GMT",
-            "Server": "sffe",
-            "X-Xss-Protection": "1; mode=block",
-            "Alternate-Protocol": "80:quic,p=0.08",
-            "Content-Length": "1442",
-            "Content-Type": "text/html; charset=UTF-8",
-            "Via": "1.1 WSA2.klm.com:80 (Cisco-WSA/8.0.6-119)",
-            "Connection": "keep-alive"
+            "content-type": "text/html; charset=UTF-8",
+            "x-content-type-options": "nosniff",
+            "date": "Mon, 23 Feb 2015 21:16:18 GMT",
+            "server": "sffe",
+            "x-xss-protection": "1; mode=block",
+            "content-length": "1435",
+            "alternate-protocol": "80:quic,p=0.08"
         },
-        "body": "<!DOCTYPE html>\n<html lang=en>\n <meta charset=utf-8>\n <meta name=viewport content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n <title>Error 404 (Not Found)!!1</title>\n <style>\n *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/errors/logo_sm_2.png) no-repeat}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/errors/logo_sm_2_hr.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/errors/logo_sm_2_hr.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:55px;width:150px}\n </style>\n Holadieeeeeeeee, dit is stap2 ^_^_^_^\n"
+        "body": "<!DOCTYPE html>\n<html lang=en>\nYahoooooo\n"
     }
 };
 
@@ -69,6 +85,11 @@ var googleScenario = googleScenarioBuilder.sequentialScenario()
     .title('Get Google Woohoo Page')
     .matchUsing(Matchers.anyMatcher)
     .respondWith(GoogleResponses.f2)
+    .then()
+    .on('yahoo')
+    .title('Get Yahoo Page')
+    .matchUsing(Matchers.ginInUrlBuilder('1234'))
+    .respondWith(GoogleResponses.f3)
     .build();
     
 config.scenarios = [googleScenario];
