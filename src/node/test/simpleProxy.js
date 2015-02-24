2136c72b4e4c9975c15ec06d6ce9d4ec51282a19
Gregory Cowan <g.cowan@amnzero.nl>
2015-02-24 12:04:57 +0100

    Simple proxy test to different file

---
+++ src/node/test/simpleProxy.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var $nocca2 = require('../index_v2');
+
+var config = {
+	endpoints: {
+		'google': {
+			targetBaseUrl: 'http://www.google.com/'
+		}
+	}
+};
+
+$nocca2.setup(config);
