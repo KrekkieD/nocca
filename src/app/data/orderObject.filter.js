80d5cc869a1c9fd95fa9f14a2498db153d821f1a
Gregory Cowan <g.cowan@amnzero.nl>
2015-03-11 14:09:26 +0100

    Fix for responding from cache, tweaked timestamp and sorting

---
+++ src/app/data/orderObject.filter.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular.module('nocca.data')
+        .filter('noccaDataOrderObject', noccaDataOrderObject);
+
+    function noccaDataOrderObject () {
+
+        return function (items, field, reverse) {
+
+            var filtered = [];
+            angular.forEach(items, function (item) {
+                filtered.push(item);
+            });
+
+            filtered.sort(function (a, b) {
+                return (a[field] > b[field] ? 1 : -1);
+            });
+
+            if (reverse) {
+                filtered.reverse();
+            }
+
+            return filtered;
+
+        };
+
+    }
+
+}());
