2cc8df20f282eec77ca93de6d03374bb4d97a017
Gregory Cowan <g.cowan@amnzero.nl>
2015-04-09 14:44:10 +0200

    WIP

---
+++ package.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var $q = require('q');
+var $npm = require('npm');
+
+var $spawn = require('child_process').spawn;
+
+
+function checkMaster (config) {
+
+    var deferred = $q.defer();
+
+    var gitBranch = $spawn('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+
+    gitBranch.stdout.on('data', function (data) {
+
+        data = data.toString().replace(/\s+/, '');
+
+        if (data !== config.primaryBranch) {
+            deferred.reject([
+                'Cannot create a package when the current branch is not ',
+                config.primaryBranch,
+                ', you are on: ',
+                data
+                ].join(' ')
+            );
+        }
+        else {
+            deferred.resolve(config);
+        }
+
+    });
+
+    return deferred.promise;
+
+}
+
+function checkGitStatus () {
+
+    var deferred = $q.defer();
+
+    var gitStatus = $spawn('git', ['status']);
+
+    gitStatus.stdout.on('data', function (data) {
+
+        data = data.toString().replace(/\s+/, '');
+
+        if (data.indexOf('working directory clean') === -1) {
+            deferred.reject('Cannot create a package when the working directory is not clean');
+        }
+        else {
+            deferred.resolve();
+        }
+
+    });
+
+    return deferred.promise;
+
+}
+
+function checkGitPull () {
+
+    var deferred = $.defer();
+
+    var gitStatus = $spawn('git', ['pull']);
+
+    gitStatus.stdout.on('data', function (data) {
+
+        data = data.toString().replace(/\s+/, '');
+
+        if (data.indexOf('Already up-to-date') === -1) {
+            deferred.reject('Working directory was not up to date');
+        }
+        else {
+            deferred.resolve();
+        }
+
+    });
+
+    return deferred.promise;
+
+}
+
+function release () {
+
+    var config = {
+        primaryBranch: 'develop'
+    };
+
+    checkMaster(config)
+        .then(checkGitStatus)
+        .then(checkGitPull)
+        .fail(function (err) {
+            console.error('ERR: ' + err);
+        });
+
+}
+
+release();
+//checkGitStatus();
