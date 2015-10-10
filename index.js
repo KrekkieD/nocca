d9b5a88d9962e74304bb5100f3aad502683e43e1
Gregory Cowan <g.cowan@amnzero.nl>
2015-10-10 19:20:49 +0200

    Implemented Bunyan logging

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
 'use strict';
 
-var _ = require('lodash');
-
 var $extend = require('extend');
 var $pubsub = require('node-pubsub');
-var $bunyan = require('bunyan');
+
+var $logger = require('./lib/bunyanLogger');
+var $utils = require('./lib/utils');
+var $constants = require('./lib/constants');
+var $defaultSettings = require('./lib/defaultSettings');
+var $pluginLoader = require('./lib/pluginLoader');
+var $playback = require('./lib/playback');
+var $wrapperServer = require('./lib/wrapperServer');
 
 module.exports = Nocca;
 
-module.exports.$cacheEntryRepository = require('./lib/cacheEntryRepository');
-module.exports.$chainBuilderFactory = require('./lib/chainBuilderFactory');
-module.exports.$constants = require('./lib/constants');
-module.exports.$defaultSettings = require('./lib/defaultSettings');
-module.exports.$errors = require('./lib/errors');
-module.exports.$forwarder = require('./lib/forwarder');
-module.exports.$logger = require('./lib/logger');
-module.exports.$patchScenarios = require('./lib/patchScenarios');
-module.exports.$recorder = require('./lib/recorder');
-module.exports.$requestExtractor = require('./lib/requestExtractor');
-module.exports.$responder = require('./lib/responder');
-module.exports.$stats = require('./lib/stats');
-module.exports.$utils = require('./lib/utils');
-module.exports.throwError = throwNoccaError;
-
-
 // the instance can carry state and allows multiple instances to run at the same time
 function Nocca (config) {
-
-    // these requires are within the Nocca instance to make sure the modules are unchanged
-    var $constants = require('./lib/constants');
-    var $defaultSettings = require('./lib/defaultSettings');
-
-    var $pluginLoader = require('./lib/pluginLoader');
-    var $utils = require('./lib/utils');
-    var $playback = require('./lib/playback');
-    var $wrapperServer = require('./lib/wrapperServer');
-
 
     // map this to self so there are no this-scope issues
     var self = this;
@@ -51,28 +30,27 @@ function Nocca (config) {
     // NOTE: pubsub is NOT configurable
     self.pubsub = $pubsub;
 
+    self.logger = new $logger(self);
+
+
     self.pluginLoader = new $pluginLoader(self);
     self.usePlugin = usePlugin;
 
     self.throwError = throwNoccaError;
+
     self.getConfig = $utils.extractConfig;
 
-    self.playback = new $playback(self);
 
     // load all plugins from config so they can be used when parsing config
     if (self.config.plugins && Array.isArray(self.config.plugins)) {
         self.pluginLoader.registerPlugins(self.config.plugins);
     }
 
-    //   C O N F I G U R A B L E   S T U F F   B E L O W
 
-    // set a logger to logger.disabled to turn off logging
-    self.log = self.config.logger;
-    self.logError = self.config.logger.error;
-    self.logWarning = self.config.logger.warning;
-    self.logSuccess = self.config.logger.success;
-    self.logInfo = self.config.logger.info;
-    self.logDebug = self.config.logger.debug;
+    self.playback = new $playback(self);
+
+
+    //   C O N F I G U R A B L E   S T U F F   B E L O W
 
 
     self.errorHandler = new self.config.errorHandler(self);
@@ -100,7 +78,7 @@ function Nocca (config) {
 
     // and mark ourselves as initialized
     self.initialized = true;
-    
+
     // run all stat reporters so they can subscribe to events. Send in the instance as arg.
     self.config.statistics.reporters.forEach(function (reporter) {
         reporter(self);
