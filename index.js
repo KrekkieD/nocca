234ee506a3ca680885bebcf67c7a79b3a186c3aa
Gregory Cowan <g.cowan@amnzero.nl>
2015-10-12 09:18:25 +0200

    Further cleanup

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 var $extend = require('extend');
 var $pubsub = require('node-pubsub');
 
-var $logger = require('./lib/bunyanLogger');
+var $bunyanLogger = require('./lib/bunyanLogger');
 var $utils = require('./lib/utils');
 var $constants = require('./lib/constants');
 var $defaultSettings = require('./lib/defaultSettings');
 var $pluginLoader = require('./lib/pluginLoader');
 var $playback = require('./lib/playback');
 var $wrapperServer = require('./lib/wrapperServer');
+
+var $forwarder = require('./lib/forwarder');
+var $recorder = require('./lib/recorder');
+var $responder = require('./lib/responder');
+var $errors = require('./lib/errors');
+var $requestContextFactory = require('./lib/requestContextFactory');
+var $httpMessageFactory = require('./lib/httpMessageFactory');
+var $chainBuilderFactory = require('./lib/chainBuilderFactory');
+var $requestExtractor = require('./lib/requestExtractor');
+var $stats = require('./lib/stats');
+
 
 module.exports = Nocca;
 
@@ -30,7 +41,7 @@ function Nocca (config) {
     // NOTE: pubsub is NOT configurable
     self.pubsub = $pubsub;
 
-    self.logger = new $logger(self);
+    self.logger = new $bunyanLogger(self, self.config.logger);
 
 
     self.pluginLoader = new $pluginLoader(self);
@@ -53,15 +64,15 @@ function Nocca (config) {
     //   C O N F I G U R A B L E   S T U F F   B E L O W
 
 
-    self.errorHandler = new self.config.errorHandler(self);
-    self.requestContextFactory = new self.config.requestContextFactory(self);
-    self.httpMessageFactory = new self.config.httpMessageFactory(self);
-    self.requestChainer = new self.config.chainBuilderFactory(self);
-    self.requestExtractor = new self.config.requestExtractor(self);
-    self.forwarder = new self.config.forwarder(self);
-    self.recorder = new self.config.recorder(self);
-    self.responder = new self.config.responder(self);
-    self.statsLogger = new self.config.statistics.instance(self);
+    self.errorHandler = new $errors(self);
+    self.requestContextFactory = new $requestContextFactory(self);
+    self.httpMessageFactory = new $httpMessageFactory(self);
+    self.requestChainer = new $chainBuilderFactory(self);
+    self.requestExtractor = new $requestExtractor(self);
+    self.forwarder = new $forwarder(self);
+    self.recorder = new $recorder(self);
+    self.responder = new $responder(self);
+    self.statsLogger = new $stats(self);
 
     // initialize repositories up front
     self.config.repositories.forEach(function (repository) {
